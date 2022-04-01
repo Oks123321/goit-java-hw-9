3f198a5d89b221c3d0339d1b570c986a0120a7e0
Oks123321 <81802094+Oks123321@users.noreply.github.com>
2022-04-01 23:06:28 +0300

    Used Maven

---
+++ src/main/java/json/UsersConverterTests.java
@@ -0,0 +1,10 @@
+package json;
+
+public class UsersConverterTests {
+    public static final String USERS_FILE_FROM = "src/main/java/file1.txt";
+    public static final String USERS_FILE_TO = "src/file1.json";
+
+    public static void main(String[] args) {
+        new UsersConverter().convert(USERS_FILE_FROM, USERS_FILE_TO);
+    }
+}
